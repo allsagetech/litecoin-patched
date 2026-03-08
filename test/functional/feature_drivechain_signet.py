a2b82dfca191ccb6115c9b17b6caeefda017fcc2
AllSage <sagesanerburrell@gmail.com>
2026-03-08 13:39:04 -0500

    test: slim signet drivechain smoke coverage

--- test/functional/feature_drivechain_signet.py
+++ test/functional/feature_drivechain_signet.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) 2026 AllSageTech, LLC
 # Distributed under the MIT software license, see COPYING.
-"""Smoke-test drivechain RPC/state flow on signet."""
-
-from decimal import Decimal
+"""Smoke-test drivechain activation and template wiring on signet."""
 
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import assert_equal
@@ -61,41 +59,25 @@ class DrivechainSignetSmokeTest(BitcoinTestFramework):
     def run_test(self):
         node = self.nodes[0]
 
-        self.log.info("mine spendable signet balance")
-        mine_addr = node.getnewaddress()
-        node.generatetoaddress(101, mine_addr, 10_000_000)
-
+        self.log.info("verify drivechain is active on signet")
         status = get_drivechain_status(node)
         assert_equal(status, "active")
 
-        owner_addr = node.getnewaddress()
-        register = node.senddrivechainregister(owner_addr)
-        sidechain_id = int(register["sidechain_id"])
-        assert_equal(sidechain_id, 0)
-        node.generatetoaddress(1, mine_addr, 10_000_000)
-
-        deposit_payload = "00" * 32
-        bundle_hash = "11" * 32
-
-        node.senddrivechaindeposit(sidechain_id, deposit_payload, [Decimal("1.0")], False)
-        node.generatetoaddress(1, mine_addr, 10_000_000)
-
-        node.senddrivechainbundle(sidechain_id, bundle_hash, owner_addr)
-        node.generatetoaddress(1, mine_addr, 10_000_000)
-
+        self.log.info("verify drivechain state RPC is available on signet")
         dcinfo = node.getdrivechaininfo()
-        sidechain = get_sidechain(dcinfo, sidechain_id)
-        assert sidechain is not None
+        assert_equal(dcinfo["sidechains"], [])
+        assert "state_cache" in dcinfo
+        assert "entries" in dcinfo["state_cache"]
+        assert "max_entries" in dcinfo["state_cache"]
 
-        assert_equal(sidechain["is_active"], True)
-        assert_equal(sidechain["escrow_balance"], 100_000_000)
-        assert_equal(sidechain["owner_auth_required"], True)
-        assert_equal(len(sidechain["bundles"]), 1)
+        self.log.info("verify getblocktemplate advertises drivechain fields on signet")
+        tmpl = node.getblocktemplate({"rules": ["mweb", "segwit"]})
+        assert "drivechain" in tmpl["rules"]
+        assert_equal(tmpl["drivechainvotes"], [])
+        assert_equal(tmpl["drivechainbmmaccepts"], [])
 
-        bundle = sidechain["bundles"][0]
-        assert_equal(bundle["hash"], bundle_hash)
-        assert_equal(bundle["approved"], False)
-        assert_equal(bundle["executed"], False)
+        self.log.info("verify no sidechains were created implicitly")
+        assert_equal(get_sidechain(node.getdrivechaininfo(), 0), None)
 
 
 if __name__ == "__main__":
