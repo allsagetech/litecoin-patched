78f40e813990b6cf3b310dbc71e50f3a2791be20
AllSage <sagesanerburrell@gmail.com>
2026-03-14 10:33:26 -0500

    Point validity wallet test at committed artifacts

--- test/functional/feature_validitysidechain_wallet.py
+++ test/functional/feature_validitysidechain_wallet.py
@@ -183,7 +183,12 @@ class ValiditySidechainWalletTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
         self.setup_clean_chain = True
-        self.extra_args = [["-acceptnonstdtxn=1"]]
+        repo_root = Path(__file__).resolve().parents[2]
+        artifact_root = repo_root / "artifacts"
+        self.extra_args = [[
+            "-acceptnonstdtxn=1",
+            f"-validityartifactsdir={artifact_root}",
+        ]]
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
